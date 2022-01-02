f7a6f3b979153b81ff7b2f294df0cd4b2ea0182b
Gio0095 <60237756+PapaMiaPizzeria@users.noreply.github.com>
2022-01-02 19:43:38 +0400

    better logger

--- vulkan_engine/src/core/log.h
+++ vulkan_engine/src/core/log.h
@@ -1,6 +1,12 @@
 #pragma once
 
+#pragma warning(push, 0)
 #include <spdlog/spdlog.h>
+#include <spdlog/fmt/ostr.h>
+#pragma warning(pop)
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include "glm/gtx/string_cast.hpp"
 
 class Log
 {
@@ -15,6 +21,24 @@ private:
     static std::shared_ptr<spdlog::logger> client_logger;
 };
 
+template <typename OStream, glm::length_t L, typename T, glm::qualifier Q>
+inline OStream &operator<<(OStream &os, const glm::vec<L, T, Q> &vector)
+{
+    return os << glm::to_string(vector);
+}
+
+template <typename OStream, glm::length_t C, glm::length_t R, typename T, glm::qualifier Q>
+inline OStream &operator<<(OStream &os, const glm::mat<C, R, T, Q> &matrix)
+{
+    return os << glm::to_string(matrix);
+}
+
+template <typename OStream, typename T, glm::qualifier Q>
+inline OStream &operator<<(OStream &os, glm::qua<T, Q> quaternion)
+{
+    return os << glm::to_string(quaternion);
+}
+
 #ifdef VE_ENABLE_DEBUG_OUTPUT
 #define VE_CORE_TRACE(...) Log::getCoreLogger()->trace(__VA_ARGS__)
 #define VE_CORE_INFO(...) Log::getCoreLogger()->info(__VA_ARGS__)
@@ -22,7 +46,7 @@ private:
 #define VE_CORE_ERROR(...) Log::getCoreLogger()->error(__VA_ARGS__)
 #define VE_CORE_CRITICAL(...) Log::getCoreLogger()->critical(__VA_ARGS__)
 #define VE_CORE_ASSERT(x, ...)      \
-    if (!x)                         \
+    if (!(x))                       \
     {                               \
         VE_CORE_ERROR(__VA_ARGS__); \
         VE_DEBUG_BREAK();           \
@@ -34,7 +58,7 @@ private:
 #define VE_ERROR(...) Log::getClientLogger()->error(__VA_ARGS__)
 #define VE_CRITICAL(...) Log::getClientLogger()->critical(__VA_ARGS__)
 #define VE_ASSERT(x, ...)      \
-    if (!x)                    \
+    if (!(x))                  \
     {                          \
         VE_ERROR(__VA_ARGS__); \
         VE_DEBUG_BREAK();      \
