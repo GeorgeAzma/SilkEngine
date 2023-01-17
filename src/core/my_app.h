d00b044a7fd5e69363e7c6a119c488951a4af21a
GeorgeAzma <gg20060705@gmail.com>
2023-01-17 04:39:47 +0400

    Main

---
+++ src/core/my_app.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "core/entry_point.h"
+#include "core/application.h"
+#include "scene/scene.h"
+
+class MyApp : public Application
+{
+public:
+    MyApp(ApplicationCommandLineArgs args);
+    void onUpdate() override;
+    ~MyApp();
+
+    std::string getName() const override { return "MyApp"; }
+
+private:
+    shared<Scene> scene;
+};
