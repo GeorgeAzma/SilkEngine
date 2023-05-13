42fb74370443484625541c150b00245ab5a8c6ce
GeorgeAzma <gg20060705@gmail.com>
2023-05-13 17:14:03 +0400

    removed render pipeline

--- silk_engine/gfx/pipeline/default_render_pipeline.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "render_pipeline.h"
-
-class DefaultRenderPipeline : public RenderPipeline
-{
-public:
-	DefaultRenderPipeline();
-};
--- silk_engine/gfx/renderer.h
+++ silk_engine/gfx/renderer.h
@@ -1,6 +1,5 @@
 #pragma once
 
-class RenderPipeline;
 class RenderPass;
 class Fence;
 class Semaphore;
@@ -15,21 +14,8 @@ public:
 	static void destroy();
 	static void wait();
 
-	template<typename T>
-	static void setRenderPipeline() 
-	{ 
-		if (render_pipeline)
-		{
-			delete render_pipeline; 
-			render_pipeline = nullptr;
-		} 
-		render_pipeline = new T(); 
-	}
-	static RenderPipeline& getRenderPipeline() { return *render_pipeline; }
-	static const RenderPass& getRenderPass(uint32_t index);
-
 private:
-	static inline RenderPipeline* render_pipeline = nullptr;
+	static inline std::vector<shared<RenderPass>> render_passes;
 	static inline Fence* previous_frame_finished = nullptr;
 	static inline Semaphore* swap_chain_image_available = nullptr;
 	static inline Semaphore* render_finished = nullptr;
